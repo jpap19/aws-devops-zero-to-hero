7c7937ef9220cb2c5d259b3abd4dd5674f327b23
Jean Passou <86032087+jpap19@users.noreply.github.com>
2025-01-08 14:42:36 -0600

    Update start_container.sh

--- scripts/start_container.sh
+++ scripts/start_container.sh
@@ -5,4 +5,5 @@ set -e
 docker pull glstudentjean20/simple-python-flask-app
 
 # Run the Docker image as a container
-docker run -d -p 5000:5000 glstudentjean20/simple-python-flask-app
+docker run -d -p 5000:5000 glstudentjean20/simple-python-flask-app 
+
